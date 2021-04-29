ed42a07952268ec94ac86693e6e0ea7fa8f3c285
John Chen <johnchen456@gmail.com>
2021-04-29 07:46:27 -0400

    fix: Fix file name and Formula name

--- Formula/boost@1.7.0.rb
+++ Formula/boost@1.7.0.rb
@@ -1,4 +1,4 @@
-class Boost < Formula
+class BoostAT170 < Formula
   desc "Collection of portable C++ source libraries"
   homepage "https://www.boost.org/"
   url "https://dl.bintray.com/boostorg/release/1.70.0/source/boost_1_70_0.tar.bz2"
