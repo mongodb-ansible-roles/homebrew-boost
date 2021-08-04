594b36e59fdc077d6307eb26671cb79b92ece9bc
Zakhar Kleyman <zakhar.kleyman@mongodb.com>
2021-08-04 17:23:36 -0400

    fix: remove bottles since they are no longer available

--- Formula/boost@1.7.0.rb
+++ Formula/boost@1.7.0.rb
@@ -5,12 +5,6 @@ class BoostAT170 < Formula
   sha256 "430ae8354789de4fd19ee52f3b1f739e1fba576f0aded0897c3c2bc00fb38778"
   head "https://github.com/boostorg/boost.git"
 
-  bottle do
-    sha256 cellar: :any, mojave:      "c626b90770424ca969d0870d25d7fb13cf9d4f23a828407701face0e7ec4ac93"
-    sha256 cellar: :any, high_sierra: "8d5a7c95155faf57ce246d3455cea3628569d684a14fb9e621893ceaf3d65373"
-    sha256 cellar: :any, sierra:      "0578344e152f306a4594b72493dcc3f638425b0fb7f4fcd23e5a523c4542b33a"
-  end
-
   depends_on "icu4c"
 
   def install
